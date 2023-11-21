82263dac901d3f2e96babdb050b8d5c36268f355
Kennedy Musau <kennedy.musauu@gmail.com>
2023-11-22 00:31:01 +0300

    update client controller with methods

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,8 +1,11 @@
 class ClientsController < ApplicationController
   def index
+    render json: Client.all
   end
 
   def show
+    client = Client.find_by(id: params[:id])
+    render json: client, status: :ok
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'clients/index'
-  get 'clients/show'
-  get 'clients/create'
-  get 'clients/update'
 
-  resources :admins,  only: [:edit, :update, :show, :destory]
+
+  resources :admins,  only: [:index]
+  resources :clients
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
