4a30e3c3e3bbfd40e28394b754b26a8b64d1d753
Kennedy Musau <kennedy.musauu@gmail.com>
2023-11-22 20:51:57 +0300

    update client controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,16 +1,42 @@
 class ClientsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  # GET /clients
   def index
     render json: Client.all
   end
 
-  def show
-    client = Client.find_by(id: params[:id])
-    render json: client, status: :ok
+  # POST /clients
+  def create
+    client = Client.create(client_params)
+    render json: client, status: :created
   end
 
-  def create
+  # GET /clients/:id
+  def show
+    client = find_client
+    render json: client
   end
+
+  # PATCH /clients/:id
 
   def update
+    client = find_client
+    client.update(client_params)
+    render json: client
+  end
+
+  private
+
+  def find_client
+    Client.find(params[:id])
+  end
+
+  def client_params
+    params.require(:client).permit(:username, :first_name, :last_name, :profile_picture, :password_digest)
+  end
+
+  def render_not_found
+    render json: { error: "Client not found"}, status: :not_found
   end
 end
